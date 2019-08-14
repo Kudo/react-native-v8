9da599b22a29271da6e94bec23fb627acd05e9a9
Kudo Chien <ckchien@gmail.com>
2019-08-14 23:22:04 +0800

    Add dedicated V8Executor and remove original JSCExecutor patch

---
+++ src/androidexecutor/reactexecutor/V8ExecutorFactory.cpp
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+
+#include "V8ExecutorFactory.h"
+
+#include <thread>
+
+#include "cxxreact/MessageQueueThread.h"
+#include "cxxreact/SystraceSection.h"
+#include "jsi/v8runtime/V8RuntimeFactory.h"
+
+using namespace facebook::jsi;
+
+namespace facebook {
+namespace react {
+
+namespace {
+
+std::unique_ptr<Runtime> makeV8RuntimeSystraced() {
+  SystraceSection s("V8ExecutorFactory::makeV8RuntimeSystraced");
+  return createV8Runtime();
+}
+
+} // namespace
+
+std::unique_ptr<JSExecutor> V8ExecutorFactory::createJSExecutor(
+    std::shared_ptr<ExecutorDelegate> delegate,
+    std::shared_ptr<MessageQueueThread> jsQueue) {
+  std::unique_ptr<Runtime> v8Runtime = makeV8RuntimeSystraced();
+
+  // Add js engine information to Error.prototype so in error reporting we
+  // can send this information.
+  auto errorPrototype = v8Runtime->global()
+                            .getPropertyAsObject(*v8Runtime, "Error")
+                            .getPropertyAsObject(*v8Runtime, "prototype");
+  errorPrototype.setProperty(*v8Runtime, "jsEngine", "v8");
+
+  return std::make_unique<V8Executor>(
+      std::move(v8Runtime),
+      delegate,
+      jsQueue,
+      timeoutInvoker_,
+      runtimeInstaller_);
+}
+
+V8Executor::V8Executor(
+    std::shared_ptr<jsi::Runtime> runtime,
+    std::shared_ptr<ExecutorDelegate> delegate,
+    std::shared_ptr<MessageQueueThread> jsQueue,
+    const JSIScopedTimeoutInvoker &timeoutInvoker,
+    RuntimeInstaller runtimeInstaller)
+    : JSIExecutor(runtime, delegate, timeoutInvoker, runtimeInstaller) {}
+
+} // namespace react
+} // namespace facebook
