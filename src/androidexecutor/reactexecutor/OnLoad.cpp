dcff1fbcc3f35f9bddf15781fc9eb65cb231074f
Kudo Chien <ckchien@gmail.com>
2019-08-14 23:22:04 +0800

    Add dedicated V8Executor and remove original JSCExecutor patch

---
+++ src/androidexecutor/reactexecutor/OnLoad.cpp
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+#include <V8ExecutorFactory.h>
+#include <fb/fbjni.h>
+#include <folly/Memory.h>
+#include <jni.h>
+#include <react/jni/JReactMarker.h>
+#include <react/jni/JSLogging.h>
+#include <react/jni/JavaScriptExecutorHolder.h>
+
+namespace facebook {
+namespace react {
+
+static void installBindings(jsi::Runtime &runtime) {
+  react::Logger androidLogger =
+      static_cast<void (*)(const std::string &, unsigned int)>(
+          &reactAndroidLoggingHook);
+  react::bindNativeLogger(runtime, androidLogger);
+}
+
+class V8ExecutorHolder
+    : public jni::HybridClass<V8ExecutorHolder, JavaScriptExecutorHolder> {
+ public:
+  static constexpr auto kJavaDescriptor =
+      "Lcom/facebook/v8/reactexecutor/V8Executor;";
+
+  static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jclass>) {
+    JReactMarker::setLogPerfMarkerIfNeeded();
+
+    return makeCxxInstance(
+        folly::make_unique<V8ExecutorFactory>(installBindings));
+  }
+
+  static void registerNatives() {
+    registerHybrid(
+        {makeNativeMethod("initHybrid", V8ExecutorHolder::initHybrid)});
+  }
+
+ private:
+  friend HybridBase;
+  using HybridBase::HybridBase;
+};
+
+} // namespace react
+} // namespace facebook
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
+  return facebook::jni::initialize(
+      vm, [] { facebook::react::V8ExecutorHolder::registerNatives(); });
+}
---
+++ src/androidexecutor/reactexecutor/V8ExecutorFactory.h
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <jsireact/JSIExecutor.h>
+
+namespace facebook {
+namespace react {
+
+class V8ExecutorFactory : public JSExecutorFactory {
+ public:
+  explicit V8ExecutorFactory(
+      JSIExecutor::RuntimeInstaller runtimeInstaller,
+      const JSIScopedTimeoutInvoker &timeoutInvoker =
+          JSIExecutor::defaultTimeoutInvoker)
+      : runtimeInstaller_(runtimeInstaller), timeoutInvoker_(timeoutInvoker) {
+    assert(timeoutInvoker_ && "Should not have empty timeoutInvoker");
+  }
+
+  std::unique_ptr<JSExecutor> createJSExecutor(
+      std::shared_ptr<ExecutorDelegate> delegate,
+      std::shared_ptr<MessageQueueThread> jsQueue) override;
+
+ private:
+  JSIExecutor::RuntimeInstaller runtimeInstaller_;
+  JSIScopedTimeoutInvoker timeoutInvoker_;
+};
+
+class V8Executor : public JSIExecutor {
+ public:
+  V8Executor(
+      std::shared_ptr<jsi::Runtime> runtime,
+      std::shared_ptr<ExecutorDelegate> delegate,
+      std::shared_ptr<MessageQueueThread> jsQueue,
+      const JSIScopedTimeoutInvoker &timeoutInvoker,
+      RuntimeInstaller runtimeInstaller);
+
+ private:
+  JSIScopedTimeoutInvoker timeoutInvoker_;
+};
+
+} // namespace react
+} // namespace facebook
