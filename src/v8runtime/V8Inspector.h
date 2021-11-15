0b5af5bc8e673ead5e1f8f4b0b0896bbdfd1f571
Kudo Chien <ckchien@gmail.com>
2021-11-15 08:47:52 +0800

    Add v8 inspector support (#91)
    - introduce `V8Inspector` class to integrate between v8_inspector and react-native `facebook::react::IRemoteConnection`
    - introduce `V8RuntimeConfig` to pass multiple configs.
    - thread-safe isolate by `v8::Locker` because `facebook::react::IRemoteConnection` will send command from other threads.

---
+++ src/v8runtime/V8Inspector.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <condition_variable>
+
+#include "jsinspector/InspectorInterfaces.h"
+#include "v8-inspector.h"
+
+namespace facebook {
+
+class InspectorClient;
+class ClientConnectionWrapper;
+
+class InspectorFrontend final : public v8_inspector::V8Inspector::Channel {
+ public:
+  explicit InspectorFrontend(
+      InspectorClient *client,
+      v8::Local<v8::Context> context);
+  ~InspectorFrontend() override = default;
+
+ private:
+  void sendResponse(
+      int callId,
+      std::unique_ptr<v8_inspector::StringBuffer> message) override;
+  void sendNotification(
+      std::unique_ptr<v8_inspector::StringBuffer> message) override;
+  void flushProtocolNotifications() override {}
+
+ private:
+  InspectorClient *client_;
+  v8::Isolate *isolate_;
+  v8::Global<v8::Context> context_;
+};
+
+class InspectorClient final
+    : public v8_inspector::V8InspectorClient,
+      public std::enable_shared_from_this<InspectorClient> {
+ public:
+  InspectorClient(
+      v8::Local<v8::Context> context,
+      const std::string &appName,
+      const std::string &deviceName);
+  ~InspectorClient() override;
+
+  void runMessageLoopOnPause(int contextGroupId) override;
+  void quitMessageLoopOnPause() override;
+  v8::Local<v8::Context> ensureDefaultContextInGroup(int group_id) override;
+
+  void ConnectToReactFrontend();
+  void Disconnect();
+  void DisconnectFromReactFrontend();
+  void SendRemoteMessage(const v8_inspector::StringView &message);
+  bool IsPaused();
+  void AwakePauseLockWithMessage(const std::string &message);
+  void DispatchProtocolMessage(const std::string &message);
+  void DispatchProtocolMessages(const std::vector<std::string> &messages);
+
+  v8::Isolate *GetIsolate();
+  v8::Global<v8::Context> &GetContext();
+  v8_inspector::V8InspectorSession *GetInspectorSession();
+
+ private:
+  static std::string CreateInspectorName(
+      const std::string &appName,
+      const std::string &deviceName);
+
+  std::weak_ptr<InspectorClient> CreateWeakPtr();
+
+ private:
+  static int nextContextGroupId_;
+
+  std::unique_ptr<v8_inspector::V8Inspector> inspector_;
+  std::unique_ptr<v8_inspector::V8InspectorSession> session_;
+  std::unique_ptr<v8_inspector::V8Inspector::Channel> channel_;
+  v8::Isolate *isolate_;
+  v8::Global<v8::Context> context_;
+
+  std::unique_ptr<react::IRemoteConnection> remoteConn_;
+  std::string inspectorName_;
+
+  std::mutex connectionMutex_;
+  std::mutex pauseMutex_;
+  std::condition_variable pauseWaitable_;
+  bool paused_ = false;
+  std::vector<std::string> protocolMessageQueue_;
+
+  int pageId_;
+  std::shared_ptr<ClientConnectionWrapper> clientConnectionWrapper_;
+};
+
+} // namespace facebook
--- src/v8runtime/V8PointerValue.cpp
+++ src/v8runtime/V8PointerValue.cpp
@@ -5,16 +5,15 @@ namespace facebook {
 V8PointerValue::V8PointerValue(
     v8::Isolate *isolate,
     const v8::Local<v8::Value> &value)
-    : value_(isolate, value) {
-}
+    : value_(isolate, value) {}
 
 V8PointerValue::~V8PointerValue() {
   value_.Reset();
 }
 
 v8::Local<v8::Value> V8PointerValue::Get(v8::Isolate *isolate) const {
-  v8::EscapableHandleScope scopedIsolate(isolate);
-  return scopedIsolate.Escape(value_.Get(isolate));
+  v8::EscapableHandleScope scopedHandle(isolate);
+  return scopedHandle.Escape(value_.Get(isolate));
 }
 
 // static
@@ -22,7 +21,7 @@ V8PointerValue *V8PointerValue::createFromOneByte(
     v8::Isolate *isolate,
     const char *str,
     size_t length) {
-  v8::HandleScope scopedIsolate(isolate);
+  v8::HandleScope scopedHandle(isolate);
   v8::Local<v8::String> v8String;
   if (!v8::String::NewFromOneByte(
            isolate,
@@ -40,7 +39,7 @@ V8PointerValue *V8PointerValue::createFromUtf8(
     v8::Isolate *isolate,
     const uint8_t *str,
     size_t length) {
-  v8::HandleScope scopedIsolate(isolate);
+  v8::HandleScope scopedHandle(isolate);
   v8::Local<v8::String> v8String;
   if (!v8::String::NewFromUtf8(
            isolate,
