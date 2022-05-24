c0449264b20640525f6d9081820fc24dbec3befa
Kudo Chien <ckchien@gmail.com>
2022-05-24 12:14:57 +0800

    update package name

--- expo/src/main/java/io/csie/kudo/reactnative/v8/expo/V8ExpoAdapterPackage.java
+++ expo/src/main/java/io/csie/kudo/reactnative/v8/expo/V8ExpoAdapterPackage.java
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package io.csie.kudo.reactnative.expo;
+package io.csie.kudo.reactnative.v8.expo;
 
 import android.content.Context;
 
