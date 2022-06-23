d108c084e9bb4b414ab8e8e10a537c81ad69fc34
Szymon Kapała <szymon20000@gmail.com>
2022-06-23 19:57:39 +0200

    Update V8Runtime.cpp

--- src/v8runtime/V8Runtime.cpp
+++ src/v8runtime/V8Runtime.cpp
@@ -108,7 +108,7 @@ V8Runtime::V8Runtime(
 
   if (config_->enableInspector) {
     inspectorClient_ = std::make_shared<InspectorClient>(
-        this->jsQueue, context_.Get(isolate_), config_->appName, config_->deviceName);
+        this->jsQueue_, context_.Get(isolate_), config_->appName, config_->deviceName);
     inspectorClient_->ConnectToReactFrontend();
   }
 }
