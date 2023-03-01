Kudo/react-native-v8

commit 3380d22428020f28cdab6d8620f9c3f740baa518

Enable bytecode caching by default (#165)
# Why

It should be safe to turn on the normal bytecode caching mode by default

# How

- deprecate experimental prebuilt codecache mode
- enable multiple file codecaching for ram bundle
- use normal cache mode by default

# Why

- ci passed
- test app running in release build with cache hit log

android/src/main/java/io/csie/kudo/reactnative/v8/executor/V8ExecutorFactory.java
@@ -68,16 +68,17 @@ public class V8ExecutorFactory implements JavaScriptExecutorFactory {
   }
 
   @Nullable
-  public static String getBundleAssetName(
+  public static String getV8BundleAssetName(
       final Context context,
+      final String assetName,
       final boolean useDeveloperSupport) {
     if (useDeveloperSupport) {
       return null;
     }
     final boolean hasCache =
-        new File(context.getCodeCacheDir(), "v8codecache.bin").exists();
+        new File(context.getCodeCacheDir(), assetName).exists();
     if (BuildConfig.V8_CACHE_MODE ==
-            V8RuntimeConfig.CODECACHE_MODE_NORMAL_WITH_STUB_BUNDLE &&
+            V8RuntimeConfig.CODECACHE_MODE_STUB_BUNDLE &&
         hasCache) {
       return "stub.bundle";
     }

android/build.gradle
@@ -87,18 +87,19 @@ if (v8AndroidVersionMajor < 11) {
 // These constant values should align to V8RuntimeConfig.java
 ext.CODECACHE_MODE_NONE = 0
 ext.CODECACHE_MODE_NORMAL = 1
-ext.CODECACHE_MODE_PREBUILT = 2
-ext.CODECACHE_MODE_NORMAL_WITH_STUB_BUNDLE = 3
+ext.CODECACHE_MODE_STUB_BUNDLE = 2
 def parseCacheMode(cacheMode) {
   switch (cacheMode) {
     case null:
+      // Use normal mode by default
+      return ext.CODECACHE_MODE_NORMAL
+    case "none":
       return ext.CODECACHE_MODE_NONE
     case "normal":
       return ext.CODECACHE_MODE_NORMAL
-    case "prebuilt":
-      return ext.CODECACHE_MODE_PREBUILT
+    case "stubBundle":
     case "normalWithStubBundle":
-      return ext.CODECACHE_MODE_NORMAL_WITH_STUB_BUNDLE
+      return ext.CODECACHE_MODE_STUB_BUNDLE
     default:
       throw new GradleException("Unsupported cache mode - ${cacheMode}")
   }
@@ -114,9 +115,6 @@ def reactNativeArchitectures() {
 }
 
 apply plugin: "com.android.library"
-if (v8CacheMode == CODECACHE_MODE_PREBUILT) {
-  apply from: "./mkcodecache.gradle"
-}
 
 android {
   compileSdkVersion safeExtGet("compileSdkVersion", 33)
@@ -188,7 +186,7 @@ android {
         assets.srcDirs += [ "${v8AndroidDir}/dist/snapshot_blob" ]
       }
 
-      if (v8CacheMode == CODECACHE_MODE_NORMAL_WITH_STUB_BUNDLE) {
+      if (v8CacheMode == CODECACHE_MODE_STUB_BUNDLE) {
         assets.srcDirs += [ "src/stub_bundle" ]
       }
     }

android/mkcodecache.gradle
@@ -1,133 +1,0 @@
-/*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- * Copyright (c) 650 Industries. All rights reserved.
- * Copyright (c) Kudo Chien.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-import java.nio.file.Paths
-import org.apache.tools.ant.taskdefs.condition.Os
-
-
-def appProjectName = findProperty('v8.appProject')
-def appProject = appProjectName ? project(":${appProjectName}") : rootProject.allprojects.find { it.plugins.hasPlugin('com.android.application') }
-if (!appProject) {
-  return;
-}
-
-File findNodePackageDir(String packageName, boolean absolute = true) {
-  def nodeCommand = ["node", "--print", "require.resolve('${packageName}/package.json')"]
-  def proc = nodeCommand.execute(null, rootDir)
-  def error = proc.err.text
-  if (error) {
-    throw new GradleException("findNodePackageDir() execution failed - nodeCommand[${nodeCommand.join(' ')}]\n" + error)
-  }
-  def dir = new File(proc.text.trim()).getParentFile()
-  return absolute ? dir.getAbsoluteFile() : dir
-}
-
-def getV8ToolsDir() {
-  def dir = findProperty('v8.android.tools.dir')
-  if (dir) {
-    return dir
-  }
-
-  def osBin = 'unsupported'
-  if (Os.isFamily(Os.FAMILY_MAC)) { osBin = 'macos' }
-  else if (Os.isOs(null, 'linux', 'amd64', null)) { osBin = 'linux' }
-  else {
-    throw new GradleException('Unsupported os for v8-android-tools')
-  }
-
-  try {
-    dir = findNodePackageDir("v8-android-tools-${osBin}").toString()
-  } catch (e) {
-    throw new GradleException("Unable to find tools package, please make sure v8-android-tools-${osBin} is installed.")
-  }
-
-  def v8AndroidDir = findProperty("v8.android.dir") ?: findV8AndroidDir()
-  def v8Variant = file(v8AndroidDir).name
-  return Paths.get(dir, v8Variant).toString()
-}
-
-def setupClosure = {
-  def appConfig = appProject.hasProperty("react") ? appProject.react : [:];
-  def bundleAssetName = appConfig.bundleAssetName ?: "index.android.bundle"
-
-  // Set bundleForVariant to a function to configure per variant,
-  // defaults to `bundleIn${targetName}` or True for Release variants and False for debug variants
-  def bundleForVariant = appConfig.bundleForVariant ?: {
-    def variant ->
-      appConfig."bundleIn${variant.name.capitalize()}" ||
-      appConfig."bundleIn${variant.buildType.name.capitalize()}" ||
-      variant.name.toLowerCase().contains("release")
-  }
-
-  def variants = appProject.android.applicationVariants
-  variants.all { def variant ->
-    def targetName = variant.name.capitalize()
-    def targetPath = variant.dirName
-    def jsBundleDir = file("${appProject.buildDir}/generated/assets/react/${targetPath}")
-
-    def currentPrebuildBundleTask = tasks.create(name: "prebuild${targetName}BundledJs") {
-      group = "rnv8"
-      description = "prebuild cached bundle into ${targetName}."
-
-      def toolsDir = getV8ToolsDir()
-
-      ['armeabi-v7a', 'arm64-v8a', 'x86', 'x86_64'].each { arch ->
-        doLast {
-          def mkcodecachePath = Paths.get(toolsDir, arch, 'mkcodecache')
-          if (!file(mkcodecachePath).exists()) {
-            logger.warn("Unsupported abi for v8-android-tools - arch[${arch}] mkcodecachePath[${mkcodecachePath}]")
-            return
-          }
-          exec {
-            workingDir jsBundleDir
-            commandLine(mkcodecachePath, bundleAssetName)
-          }
-          ant.move file: Paths.get(jsBundleDir.toString(), 'v8codecache.bin'), todir: Paths.get(jsBundleDir.toString(), arch)
-          copy {
-            from(Paths.get(jsBundleDir.toString(), arch))
-            // for AGP 3.4+ new asset directory
-            into("${appProject.buildDir}/intermediates/merged_assets/${variant.name}/out/${arch}")
-          }
-        }
-      }
-
-      if (variant.ext.has('bundleJsAndAssets')) {
-        dependsOn(variant.ext.bundleJsAndAssets)
-        enabled bundleForVariant(variant)
-      } else {
-        enabled false
-      }
-    }
-
-    def currentWipeBundleTask = tasks.create(name: "wipe${targetName}BundledJs") {
-      group = "rnv8"
-      description = "wipe bundle from ${targetName}."
-
-      doLast {
-        file(Paths.get(jsBundleDir.toString(), bundleAssetName)).text = "// @generated stub bundle";
-      }
-
-      dependsOn(currentPrebuildBundleTask)
-      enabled(currentPrebuildBundleTask.enabled)
-    }
-
-    def task = appProject.tasks.findByName("copy${targetName}BundledJs")
-    if (task) {
-      task.dependsOn(currentWipeBundleTask)
-    }
-  }
-}
-
-if (appProject.state.hasCompleted()) {
-  setupClosure.call()
-} else {
-  appProject.afterEvaluate {
-    setupClosure.call()
-  }
-}

android/src/main/cpp/OnLoad.cpp
@@ -70,7 +70,7 @@ class V8ExecutorHolder
       const std::string &deviceName,
       const std::string &snapshotBlobPath,
       int codecacheMode,
-      const std::string &codecachePath) {
+      const std::string &codecacheDir) {
     react::JReactMarker::setLogPerfMarkerIfNeeded();
 
     auto config = std::make_unique<V8RuntimeConfig>();
@@ -84,12 +84,7 @@ class V8ExecutorHolder
     }
     config->codecacheMode =
         static_cast<V8RuntimeConfig::CodecacheMode>(codecacheMode);
-    config->codecachePath = codecachePath;
-    if (config->codecacheMode == V8RuntimeConfig::CodecacheMode::kPrebuilt &&
-        !codecachePath.empty()) {
-      config->prebuiltCodecacheBlob =
-          std::move(loadBlob(assetManager, codecachePath));
-    }
+    config->codecacheDir = codecacheDir;
 
     return makeCxxInstance(folly::make_unique<V8ExecutorFactory>(
         installBindings,

android/src/main/java/io/csie/kudo/reactnative/v8/executor/V8Executor.java
@@ -32,9 +32,8 @@ public class V8Executor extends JavaScriptExecutor {
         config.snapshotBlobPath != null ? config.snapshotBlobPath
                                         : loadDefaultSnapshotBlobPath(),
         config.codecacheMode,
-        config.codecachePath != null
-            ? config.codecachePath
-            : loadDefaultCodecachePath(context, config.codecacheMode)));
+        config.codecacheDir != null ? config.codecacheDir
+                                    : context.getCodeCacheDir().toString()));
   }
 
   @Override
@@ -49,15 +48,6 @@ public class V8Executor extends JavaScriptExecutor {
     return "";
   }
 
-  private static String loadDefaultCodecachePath(
-      final Context context,
-      int codecacheMode) {
-    if (codecacheMode == V8RuntimeConfig.CODECACHE_MODE_PREBUILT) {
-      return "assets://" + Build.SUPPORTED_ABIS[0] + "/v8codecache.bin";
-    }
-    return new File(context.getCodeCacheDir(), "v8codecache.bin").toString();
-  }
-
   private static native HybridData initHybrid(
       AssetManager assetManager,
       String timezoneId,
@@ -66,7 +56,7 @@ public class V8Executor extends JavaScriptExecutor {
       String deviceName,
       String snapshotBlobPath,
       int codecacheMode,
-      String codecachePath);
+      String codecacheDir);
 
   /* package */ static native void onMainLoopIdle(
       RuntimeExecutor runtimeExecutor);

android/src/main/java/io/csie/kudo/reactnative/v8/executor/V8RuntimeConfig.java
@@ -32,10 +32,7 @@ public final class V8RuntimeConfig {
   @Nullable public String snapshotBlobPath;
 
   @IntDef(
-      {CODECACHE_MODE_NONE,
-       CODECACHE_MODE_NORMAL,
-       CODECACHE_MODE_PREBUILT,
-       CODECACHE_MODE_NORMAL_WITH_STUB_BUNDLE})
+      {CODECACHE_MODE_NONE, CODECACHE_MODE_NORMAL, CODECACHE_MODE_STUB_BUNDLE})
   @Retention(RetentionPolicy.SOURCE)
   private @interface CodecacheMode {}
 
@@ -43,17 +40,15 @@ public final class V8RuntimeConfig {
   public static final int CODECACHE_MODE_NONE = 0;
   // Classic v8 bytecode caching
   public static final int CODECACHE_MODE_NORMAL = 1;
-  // **EXPERIMENTAL** Prebuilt bytecode caching
-  public static final int CODECACHE_MODE_PREBUILT = 2;
   // **EXPERIMENTAL** Classic v8 bytecode caching + loading stub JS bundle when
   // cache existed
-  public static final int CODECACHE_MODE_NORMAL_WITH_STUB_BUNDLE = 3;
+  public static final int CODECACHE_MODE_STUB_BUNDLE = 2;
 
   // Bytecode caching mode
   public @CodecacheMode int codecacheMode;
 
-  // Codecache blob
-  @Nullable public String codecachePath;
+  // The directory to store codecache files
+  @Nullable public String codecacheDir;
 
   public static V8RuntimeConfig createDefault() {
     final V8RuntimeConfig config = new V8RuntimeConfig();
@@ -61,7 +56,7 @@ public final class V8RuntimeConfig {
     config.enableInspector = false;
     config.snapshotBlobPath = null;
     config.codecacheMode = CODECACHE_MODE_NONE;
-    config.codecachePath = null;
+    config.codecacheDir = null;
     return config;
   }
 

src/v8runtime/V8Runtime.cpp
@@ -8,6 +8,7 @@
 #include "V8Runtime.h"
 
 #include <glog/logging.h>
+#include <filesystem>
 #include <mutex>
 #include <sstream>
 #include "HostProxy.h"
@@ -176,7 +177,7 @@ jsi::Value V8Runtime::ExecuteScript(
 
   v8::Local<v8::Context> context(isolate->GetCurrentContext());
 
-  auto codecache = LoadCodeCacheIfNeeded(config_->codecachePath);
+  auto codecache = LoadCodeCacheIfNeeded(sourceURL);
   v8::ScriptCompiler::CachedData *cachedData = codecache.release();
 
   std::unique_ptr<v8::ScriptCompiler::Source> source =
@@ -198,9 +199,9 @@ jsi::Value V8Runtime::ExecuteScript(
   }
 
   if (cachedData && cachedData->rejected) {
-    LOG(INFO) << "[rnv8] cache missed.";
+    LOG(INFO) << "[rnv8] cache miss: " << sourceURL;
   }
-  SaveCodeCacheIfNeeded(compiledScript, config_->codecachePath, cachedData);
+  SaveCodeCacheIfNeeded(compiledScript, sourceURL, cachedData);
 
   v8::Local<v8::Value> result;
   if (!compiledScript->Run(context).ToLocal(&result)) {
@@ -263,7 +264,7 @@ void V8Runtime::ReportException(v8::Isolate *isolate, v8::TryCatch *tryCatch)
 }
 
 std::unique_ptr<v8::ScriptCompiler::CachedData>
-V8Runtime::LoadCodeCacheIfNeeded(const std::string &codecachePath) {
+V8Runtime::LoadCodeCacheIfNeeded(const std::string &sourceURL) {
   // caching is for main runtime only
   if (isSharedRuntime_) {
     return nullptr;
@@ -273,27 +274,20 @@ V8Runtime::LoadCodeCacheIfNeeded(const std::string &codecachePath) {
     return nullptr;
   }
 
-  if (config_->codecacheMode == V8RuntimeConfig::CodecacheMode::kPrebuilt) {
-    assert(config_->prebuiltCodecacheBlob);
-    return std::make_unique<v8::ScriptCompiler::CachedData>(
-        reinterpret_cast<const uint8_t *>(
-            config_->prebuiltCodecacheBlob->c_str()),
-        static_cast<int>(config_->prebuiltCodecacheBlob->size()),
-        v8::ScriptCompiler::CachedData::BufferPolicy::BufferNotOwned);
-  }
-
-  FILE *file = fopen(codecachePath.c_str(), "rb");
+  std::filesystem::path codecachePath(config_->codecacheDir);
+  codecachePath /= std::filesystem::path(sourceURL).filename();
+  auto *file = std::fopen(codecachePath.string().c_str(), "rb");
   if (!file) {
-    LOG(INFO) << "Cannot load codecache file: " << codecachePath;
+    LOG(INFO) << "Cannot load codecache file: " << codecachePath.string();
     return nullptr;
   }
-  fseek(file, 0, SEEK_END);
-  size_t size = ftell(file);
+  std::fseek(file, 0, SEEK_END);
+  size_t size = std::ftell(file);
   uint8_t *buffer = new uint8_t[size];
-  rewind(file);
+  std::rewind(file);
 
-  fread(buffer, size, 1, file);
-  fclose(file);
+  std::fread(buffer, size, 1, file);
+  std::fclose(file);
 
   return std::make_unique<v8::ScriptCompiler::CachedData>(
       buffer,
@@ -303,7 +297,7 @@ V8Runtime::LoadCodeCacheIfNeeded(const std::string &codecachePath) {
 
 bool V8Runtime::SaveCodeCacheIfNeeded(
     const v8::Local<v8::Script> &script,
-    const std::string &codecachePath,
+    const std::string &sourceURL,
     v8::ScriptCompiler::CachedData *cachedData) {
   // caching is for main runtime only
   if (isSharedRuntime_) {
@@ -314,8 +308,7 @@ bool V8Runtime::SaveCodeCacheIfNeeded(
     return false;
   }
 
-  if (config_->codecacheMode == V8RuntimeConfig::CodecacheMode::kNone ||
-      config_->codecacheMode == V8RuntimeConfig::CodecacheMode::kPrebuilt) {
+  if (config_->codecacheMode == V8RuntimeConfig::CodecacheMode::kNone) {
     return false;
   }
 
@@ -328,14 +321,16 @@ bool V8Runtime::SaveCodeCacheIfNeeded(
     return false;
   }
 
-  FILE *file = fopen(codecachePath.c_str(), "wb");
-  if (!file) {
-    LOG(ERROR) << "Cannot save codecache file: " << codecachePath;
+  std::filesystem::path codecachePath(config_->codecacheDir);
+  codecachePath /= std::filesystem::path(sourceURL).filename();
+  if (auto *file = std::fopen(codecachePath.string().c_str(), "wb")) {
+    std::fwrite(newCachedData->data, 1, newCachedData->length, file);
+    std::fclose(file);
+    return true;
+  } else {
+    LOG(ERROR) << "Cannot save codecache file: " << codecachePath.string();
     return false;
   }
-  fwrite(newCachedData->data, 1, newCachedData->length, file);
-  fclose(file);
-  return true;
 }
 
 std::unique_ptr<v8::ScriptCompiler::Source> V8Runtime::UseFakeSourceIfNeeded(
@@ -350,9 +345,7 @@ std::unique_ptr<v8::ScriptCompiler::Source> V8Runtime::UseFakeSourceIfNeeded(
     return nullptr;
   }
 
-  if (config_->codecacheMode == V8RuntimeConfig::CodecacheMode::kPrebuilt ||
-      config_->codecacheMode ==
-          V8RuntimeConfig::CodecacheMode::kNormalWithStubBundle) {
+  if (config_->codecacheMode == V8RuntimeConfig::CodecacheMode::kStubBundle) {
     uint32_t payloadSize = (cachedData->data[8] << 0) |
         (cachedData->data[9] << 8) | (cachedData->data[10] << 16) |
         (cachedData->data[11] << 24);
@@ -961,8 +954,9 @@ void V8Runtime::setNativeState(
               v8::String::NewFromUtf8Literal(isolate_, "assign"))
           .ToLocalChecked());
   v8::Local<v8::Value> args[] = {v8Object, v8ObjectOriginal};
-  objectAssignFunction->Call(
-      isolate_->GetCurrentContext(), v8::Undefined(isolate_), 2, args);
+  objectAssignFunction
+      ->Call(isolate_->GetCurrentContext(), v8::Undefined(isolate_), 2, args)
+      .ToLocalChecked();
 
   // Bind a global handle with weak callback to cleanup the shared_ptr
   // on the C++ heap.

src/v8runtime/V8Runtime.h
@@ -41,10 +41,10 @@ class V8Runtime : public facebook::jsi::Runtime {
   void ReportException(v8::Isolate *isolate, v8::TryCatch *tryCatch) const;
 
   std::unique_ptr<v8::ScriptCompiler::CachedData> LoadCodeCacheIfNeeded(
-      const std::string &codecachePath);
+      const std::string &sourceURL);
   bool SaveCodeCacheIfNeeded(
       const v8::Local<v8::Script> &script,
-      const std::string &codecachePath,
+      const std::string &sourceURL,
       v8::ScriptCompiler::CachedData *cachedData);
   std::unique_ptr<v8::ScriptCompiler::Source> UseFakeSourceIfNeeded(
       const v8::ScriptOrigin &origin,

commit e3a3b4c3c91a001af97373a0dbe3c7fde6e24a38

Fix expo integration on android (#170)
# Why

fixes #168

# How

- this is a regression from #165. i would add the original `getBundleAssetName` back.
- add expo android project ci job

# Test Plan

ci passed

.github/workflows/android.yml
@@ -1,4 +1,4 @@
-name: E2E tests for Android
+name: E2E tests on bare React Native Android project
 
 on:
   push:
@@ -52,11 +52,13 @@ jobs:
       - name: Run tests
         uses: reactivecircus/android-emulator-runner@v2
         with:
-          api-level: 29
+          api-level: 33
+          arch: x86_64
+          target: google_apis
           script: |
             adb logcat -c
             python3 scripts/gen_project.py --rn-version ${{ matrix.rn-version }} --v8-android-variant ${{ matrix.v8-android-variant }} TestApp
-            sleep 5
+            sleep 10
             adb logcat -d > TestApp/adb.log
             grep -E "=== V8 version\[.+\] ===" TestApp/adb.log > /dev/null
 

.github/workflows/expo-android.yml
@@ -1,0 +1,85 @@
+name: E2E tests on Expo Android project
+
+on:
+  push:
+    branches: [main, '*-stable']
+    paths:
+      - .github/workflows/expo-android.yml
+      - android/**
+      - src/v8runtime/**
+      - package.json
+      - yarn.lock
+  pull_request:
+    paths:
+      - .github/workflows/expo-android.yml
+      - android/**
+      - src/v8runtime/**
+      - package.json
+      - yarn.lock
+
+jobs:
+  e2e-test:
+    runs-on: macos-latest
+
+    steps:
+      - uses: actions/checkout@v2
+
+      - name: Use JDK 11
+        uses: actions/setup-java@v2
+        with:
+          distribution: 'temurin'
+          java-version: '11'
+
+      - name: Restore yarn caches
+        uses: actions/cache@v2
+        with:
+          path: node_modules
+          key: ${{ runner.os }}-workspace-modules-${{ hashFiles('yarn.lock') }}
+
+      - name: Yarn install
+        run: yarn install --frozen-lockfile
+
+      - name: Generate TestApp
+        run: |
+          yarn create expo-app TestApp
+          cd TestApp
+          yarn add v8-android-jit
+          yarn add file:../react-native-v8
+          jq '.expo.android.package = "com.testapp" | .expo.plugins += ["react-native-v8"] | .expo.jsEngine = "jsc"' app.json > app.json.tmp && mv -f app.json.tmp app.json
+          echo 'if (global._v8runtime) { console.log(`=== V8 version[${global._v8runtime().version}] ===`); }' >> App.js
+        working-directory: ..
+
+      - name: Build TestApp
+        working-directory: ../TestApp
+        run: |
+          npx expo prebuild -p android
+          cd android
+          ./gradlew :app:assembleRelease
+
+      - name: Run tests
+        uses: reactivecircus/android-emulator-runner@v2
+        with:
+          api-level: 33
+          arch: x86_64
+          target: google_apis
+          working-directory: ../TestApp/android
+          script: |
+            adb install -r app/build/outputs/apk/release/app-release.apk
+            adb logcat -c
+            adb shell am start -n com.testapp/com.testapp.MainActivity
+            sleep 10
+            adb logcat -d > adb.log
+            grep -E "=== V8 version\[.+\] ===" adb.log > /dev/null
+
+      - name: Collect failure files
+        if: failure()
+        run: |
+          cp ../TestApp/android/adb.log .
+
+      - name: Upload failed artifacts
+        if: failure()
+        uses: actions/upload-artifact@v3
+        with:
+          name: artifacts
+          path: |
+            adb.log

.github/workflows/expo-ios.yml
@@ -1,17 +1,17 @@
-name: E2E tests for iOS
+name: E2E tests on Expo iOS project
 
 on:
   push:
     branches: [main, '*-stable']
     paths:
-      - .github/workflows/ios.yml
+      - .github/workflows/expo-ios.yml
       - src/iosexecutor/**
       - src/v8runtime/**
       - package.json
       - yarn.lock
   pull_request:
     paths:
-      - .github/workflows/ios.yml
+      - .github/workflows/expo-ios.yml
       - src/iosexecutor/**
       - src/v8runtime/**
       - package.json
@@ -73,6 +73,6 @@ jobs:
         if: failure()
         uses: actions/upload-artifact@v3
         with:
-          name: artifacts-${{ matrix.rn-version }}-${{ matrix.v8-android-variant }}
+          name: artifacts
           path: |
             sim.log

android/src/main/java/io/csie/kudo/reactnative/v8/executor/V8ExecutorFactory.java
@@ -84,4 +84,16 @@ public class V8ExecutorFactory implements JavaScriptExecutorFactory {
     }
     return null;
   }
+
+  /**
+   * For Expo integration where we cannot get exact `getBundleAssetName`,
+   * use a hardcoded **index.android.bundle** default name instead.
+   */
+  @Nullable
+  public static String getBundleAssetName(
+      final Context context,
+      final boolean useDeveloperSupport) {
+    return getV8BundleAssetName(
+        context, "index.android.bundle", useDeveloperSupport);
+  }
 }