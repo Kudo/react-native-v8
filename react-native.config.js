7e0f979dd2dae44b4c8ba104a0280010fc372c48
Kudo Chien <ckchien@gmail.com>
2023-07-02 15:48:22 +0800

    fix

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,16 @@
+const v8IosInstalled = (function() {
+ try {
+    return !!require.resolve('v8-ios/package.json');
+ } catch {
+    return false;
+ }
+})();
+
 module.exports = {
   dependency: {
     platforms: {
       // Only enable ios autolinking when v8-ios is installed
-      ios: !!require.resolve('v8-ios/package.json'),
+      ios: v8IosInstalled ? {} : null,
     },
   },
 };
